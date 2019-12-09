1f5e988b4d3697d8fcd5fc41ed444097eacce21b
zq <robotmam.top@gmail.com>
2019-12-09 21:50:09 +0800

    #1.0

---
+++ app/src/main/java/com/redhood/hoolicalendar/MainActivity.java
@@ -0,0 +1,56 @@
+package com.redhood.hoolicalendar;
+
+
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        initCompent();
+
+    }
+
+    private void initCompent() {
+        //初始化toolbar
+//        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
+        //设置底部导航
+
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setItemIconSize(70);
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) bottomNav.getChildAt(0);
+        menuView.scrollBy(0,10);
+        ImageView imageView = menuView.getChildAt(2).findViewById(com.google.android.material.R.id.icon);
+//        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(80,80);
+        imageView.getLayoutParams().width = 140;
+        imageView.getLayoutParams().height = 140;
+        imageView.scrollBy(0,8);
+
+//设置模式
+        bottomNav.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_container);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+    }
+}
