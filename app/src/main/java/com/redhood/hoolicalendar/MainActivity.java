c76092ded27df812b8733482b42d5dcae4ddd00a
redhood <2043570808@qq.com>
2019-12-17 22:49:29 +0800

    #ADD_icon

--- app/src/main/java/com/redhood/hoolicalendar/MainActivity.java
+++ app/src/main/java/com/redhood/hoolicalendar/MainActivity.java
@@ -1,72 +1,93 @@
 package com.redhood.hoolicalendar;
 
 
-
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 
-import com.alibaba.fastjson.JSON;
-import com.android.volley.Request;
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 import com.redhood.hoolicalendar.fragment.PersonFragment;
-import com.redhood.hoolicalendar.utils.ImmersiveStatusBarSettings;
-
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNav;
     NavController navController;
+    PopupWindow popupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-//        new ImmersiveStatusBarSettings().settingStatusBar(this);
 
         initCompent();
     }
 
     private void initCompent() {
-        //初始化toolbar
-//        Toolbar myToolbar = findViewById(R.id.my_toolbar);
-//        setSupportActionBar(myToolbar);
         //设置底部导航
-         bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setItemIconSize(60);
-
         BottomNavigationMenuView menuView = (BottomNavigationMenuView) bottomNav.getChildAt(0);
-        menuView.scrollBy(0,8);
+        menuView.scrollBy(0, 8);
 //        bottomNav.getMenu().findItem(R.id.navigation_add).setIcon(R.mipmap.add_icon);
         bottomNav.setItemIconTintList(null);
 //        bottomNav.setText
-        ImageView imageView = menuView.getChildAt(2).findViewById(com.google.android.material.R.id.icon);
+        ImageView addIcon = menuView.getChildAt(2).findViewById(com.google.android.material.R.id.icon);
 //        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(80,80);
-        imageView.getLayoutParams().width = 110;
-        imageView.getLayoutParams().height = 110;
-        imageView.scrollBy(0,0);
+        addIcon.getLayoutParams().width = 110;
+        addIcon.getLayoutParams().height = 110;
+        addIcon.scrollBy(0, 0);
+        addIcon.setClickable(true);
+        addIcon.setOnClickListener(v -> {
+            showPopUpWindow();
+        });
 
 //设置模式
         bottomNav.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
 
         navController = Navigation.findNavController(this, R.id.nav_host_container);
         NavigationUI.setupWithNavController(bottomNav, navController);
+    }
 
+    private void showPopUpWindow() {
+        popupWindow = new PopupWindow(this);
+        popupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+        popupWindow.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
+        View popup_add_icon = LayoutInflater.from(this).inflate(R.layout.popup_add_icon,null);
+        popupWindow.setContentView(popup_add_icon);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
+//        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setFocusable(true);
+        Log.e("ss", "showPopUpWindow: a");
+        //引入依附的布局
+//        View parentView = LayoutInflater.from(MainActivity.this).inflate(R.layout.activity_home, null);
+        popupWindow.showAtLocation((View)bottomNav.getParent(), Gravity.TOP,0,0);
+        LinearLayout llayout_popup = popup_add_icon.findViewById(R.id.llayout_popup);
+        ImageView iv_icon = popup_add_icon.findViewById(R.id.iv_icon);
+        llayout_popup.setOnClickListener(v -> {
+            Log.e("ss", "showPopUpWindow: " );
+            popupWindow.dismiss();
+        });
+//        iv_icon.setOnClickListener(v -> {
+//            Log.e("ss", "sb: " );
+//        });
     }
 
     /**
@@ -77,4 +98,6 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         PersonFragment.myInformation = null;
     }
+
+
 }
