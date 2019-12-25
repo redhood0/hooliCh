9e06e194e536a8267a8617c562d695a78cfcc352
chenkeyu <chenkeyu@qq.com>
2019-12-25 09:51:50 +0800

    #12.25 token

--- app/src/main/java/com/redhood/hoolicalendar/fragment/TweetMyFragment.java
+++ app/src/main/java/com/redhood/hoolicalendar/fragment/TweetMyFragment.java
@@ -20,7 +20,7 @@ public class TweetMyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_tweet_new,container,false);
+        View view = inflater.inflate(R.layout.fragment_tweet_my,container,false);
         return view;
     }
     private void init(View view){
